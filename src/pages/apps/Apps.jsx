51d9a6faeedc8c78471ee45db992d1973fd48ea0
ts-webdev <dev.tanvirmahtab@gmail.com>
2025-10-09 01:17:19 +0600

    Complete search app functionality

--- src/pages/apps/Apps.jsx
+++ src/pages/apps/Apps.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useAppData from '../../hooks/useAppData';
 import AppCard from '../../components/appCard/AppCard';
 
 const Apps = () => {
     const { appData } = useAppData();
+
+    const [search, setSearch] = useState('')
+    const term = search.trim().toLocaleLowerCase()
+    const searchData = term? appData.filter(matchData => matchData.title.toLocaleLowerCase().includes(term)) : appData
     return (
         <div className='bg-[#d2d2d240]'>
             <div className='container mx-auto py-20'>
                 <h1 className='text-4xl font-bold text-center text-[#001931]'>Our All Applications</h1>
                 <p className='text-[#627382] text-center'>Explore All Apps on the Market developed by us. We code for Millions</p>
                 <div className='flex justify-between mt-10'>
-                    <p className='text-2xl font-semibold text-[#001931]'>({appData.length})Apps Found</p>
+                    <p className='text-2xl font-semibold text-[#001931]'>({searchData.length})Apps Found</p>
                     <label>
-                        <input type="search" placeholder="Type here" className="input" />
+                        <input value={search} onChange={(e)=> setSearch(e.target.value)} type="search" placeholder="Type here" className="input" />
                     </label>
                 </div>
                 <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10 px-5'>
                     {
-                        appData.map(singleData => <AppCard key={singleData.id} singleData={singleData}></AppCard>)
+                        searchData.map(singleData => <AppCard key={singleData.id} singleData={singleData}></AppCard>)
                     }
                 </div>
             </div>
